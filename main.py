3789442f3df8c9f5b68e7b7290c452eb1c82a1bd
leegn4 <94218239+leegn4@users.noreply.github.com>
2021-12-23 14:02:43 +0000

    Add comments

--- main.py
+++ main.py
@@ -2,51 +2,51 @@ import os
 import win32file
 from cryptography.fernet import Fernet
 
-keys={}
+keys={} # dictionary 사용하여 field,usr,pwd 저장
 
-class F():
+class F(): # 암호 관련 클래스 생성
     def __init__(self, key=None) -> None:
         if os.name=="nt":
-            key_name="p.key"
+            key_name="p.key" # Windows인 경우 key filename
         else:
-            key_name=".pkey"
-        if not os.path.isfile(key_name):
+            key_name=".pkey" # Linux 인 경우 key filename, hidden
+        if not os.path.isfile(key_name): # key가 존재하지 않으면 생성
             key=Fernet.generate_key()
             with open(key_name,"wb") as f:
                 f.write(key)
-            if os.name=="nt":
+            if os.name=="nt": # hidden file(Windows)
                 win32file.SetFileAttributes(key_name,2)
         else:
-            key=open(key_name,"rb").read()
+            key=open(key_name,"rb").read() # key가 존재하면 읽음
         self.key=key
         self.f=Fernet(self.key)
     
-    def encrypt(self, data, is_string=True):
-        if isinstance(data,bytes):
+    def encrypt(self, data, is_string=True): # 암호화 method
+        if isinstance(data,bytes): # bytes 인 경우 그대로 암호화
             res=self.f.encrypt(data)
         else:
-            res=self.f.encrypt(data.encode("utf-8"))
+            res=self.f.encrypt(data.encode("utf-8")) # 문자열이면 인코딩 후 암호화
         if is_string:
-            return res.decode("utf-8")
+            return res.decode("utf-8") # 문자열 출력 시 디코딩
         else:
             return res
     
-    def decrypt(self, data, is_string=True):
-        if isinstance(data,bytes):
+    def decrypt(self, data, is_string=True): # 복호화 method
+        if isinstance(data,bytes): # bytes 인 경우 그대로 복호화
             res=self.f.decrypt(data)
         else:
-            res=self.f.decrypt(data.encode("utf-8"))
+            res=self.f.decrypt(data.encode("utf-8")) # 문자열이면 인코딩 후 복호화
         if is_string:
-            return res.decode("utf-8")
+            return res.decode("utf-8") # 문자열 출력 시 디코딩
         else:
             return res
 
-password=F()
+password=F() # password 객체 생성
 
-def clear():
+def clear(): # clear prompt
     os.system("cls" if os.name=="nt" else "clear")
 
-def Add():
+def Add(): # 필드 추가 함수
     while True:
         field_name=input("Field Name: ").strip()
         if field_name=="" or field_name.isspace(): continue
@@ -62,7 +62,7 @@ def Add():
     content=(user_name,pwd)
     keys[field_name]=content
 
-def Remove():
+def Remove(): # 필드 제거 함수
     field=input("Key name to remove: ").strip()
     if keys.get(field) != None:
         del keys[field]
@@ -70,13 +70,13 @@ def Remove():
     else:
         print("Not valid key name")
 
-def Search():
+def Search(): # 필드 검색 함수
     while True:
         src=input("Key name or username (* to show all): ").strip()
         if src=="" or src.isspace(): continue
         else: break
     found=False
-    if src=="*":
+    if src=="*": # wildcard 입력일 때 전체 출력
         for field in keys:
             i=keys[field]
             print(f"Key: {field}")
@@ -84,7 +84,7 @@ def Search():
             print(f"Password: {i[1]}")
             print()
         return
-    for field in keys:
+    for field in keys: # 부분 문자열로 검색하여 출력
         if (src in field) or (src in keys[field][0]):
             i=keys[field]
             print(f"Key: {field}")
@@ -95,7 +95,7 @@ def Search():
     if not found:
         print("No key found")
 
-def Save(f):
+def Save(f): # 종료 시 passwd 파일에 딕셔너리 저장
     for field in keys:
         fld=field
         usr,pwd=keys[field]
@@ -103,8 +103,8 @@ def Save(f):
         f.write(f"{fld},{usr},{pwd}\n")
     f.close()
 
-def Load(f):
-    f.seek(0)
+def Load(f): # 실행 시 passwd 파일로 딕셔너리 초기화
+    f.seek(0) # append 이므로 파일 포인터 처음으로
     for line in f:
         fld,usr,pwd=line.strip().split(",")
         try:
@@ -115,7 +115,7 @@ def Load(f):
         keys[fld]=content
 
 def main():
-    f=open("passwd","a+t")
+    f=open("passwd","a+t") # 파일이 있다면 그대로 읽기, 없다면 생성
     Load(f)
     f.close()
     while True:
@@ -137,7 +137,7 @@ def main():
             clear()
             Search()
         elif i=="4":
-            f=open("passwd","wt")
+            f=open("passwd","wt") # 저장 시 passwd 파일 덮어쓰기
             Save(f)
             exit()
         else:
